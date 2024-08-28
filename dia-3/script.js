adc88b2f6ba74469813b4c2d7c3fbeb8241553d4
Andre Vieira <andrevieira19992018@gmail.com>
2024-08-28 13:01:30 -0300

    adicionando arquivos dia 3 - não finalizado

---
+++ dia-3/script.js
@@ -0,0 +1,20 @@
+let escolhaUsuario = Number(prompt('Escolha qual área de TI deseja seguir: \n (1) Front-End \n (2) Back-End'));
+
+
+
+if(escolhaUsuario === 1){
+    let escolhaFrontEnd = Number(prompt('------ Escolha FRONT-END ------ \n Agora escolha o que deseja aprender: \n (1) React \n (2) Vue'));
+
+}else if(escolhaUsuario ===2){
+    let escolhaFrontEnd = Number(prompt('------ Escolha BACK-END ------ \n Agora escolha o que deseja aprender: \n (1) C# \n (2) Java')); 
+}
+
+let escolhaFuturaUsuario = Number(prompt('Você deseja continuar na área ? \n (1) Sim \n (2) Não, vou escolher ser um desenvolvedor Fullstack'));
+
+let tecnologias = [];
+let continuar = true;
+ while(true){
+    let resposta = prompt('Digite quais tecnologis gostaria de se especializar ou de conhecer:');
+
+
+ }
